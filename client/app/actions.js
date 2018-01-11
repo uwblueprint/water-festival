352733572e7b083e75e79ee5b77310bbf0d27ab0
raphael <raph.koh@gmail.com>
2018-01-10 20:24:56 -0500

    Move files to app folder

---
+++ client/app/actions.js
@@ -0,0 +1,14 @@
+/*
+ * action types
+ */
+
+export const FAQ_LOADED = 'FAQ_LOADED';
+
+
+/*
+ * action creators
+ */
+
+export const faqLoaded = (faqList) => {
+	return { type: FAQ_LOADED, faqList };
+};
