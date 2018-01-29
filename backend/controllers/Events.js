3dfeb959f94249bba03a7e1186173f0c36ce031c
Amy Qiu <amy-qiu@outlook.com>
2018-01-28 22:47:10 -0500

    Add endpoints for post/add/delete of festival events

--- backend/controllers/Events.js
+++ backend/controllers/Events.js
@@ -14,67 +14,61 @@ eventRouter.use(bodyParser.urlencoded({
 
 eventRouter.get('/list', function(req, res) {
   Event.find(function(err, events) {
-    if (err) {
-      res.json(err);
-    }
-    var mappedEvents = events.map(q => q.toJSONFor());
-    res.json(mappedEvents);
-  });
+		if (err) {
+			res.json(err);
+		}
+		events = events.map(q => q.toJSONFor());
+		res.json(events);
+	});
 })
 
 eventRouter.delete('/delete', function(req, res) {
-  var ids = req.body.eventIDs.map(function(id) {
-    return new mongodb.ObjectID(id);
-  });
+	var ids = req.body.eventIDs.map(function(id) {
+		return new mongodb.ObjectID(id);
+	});
 
-  Event.deleteMany({
-    _id: {
-      $in: ids
-    }
-  }, function(err) {
-    if (err) {
-      res.send(err);
-    }
+	Event.deleteMany({_id: {$in: ids}}, function(err) {
+		if (err) {
+			res.send(err);
+		}
   });
-  res.send({
-    "message": "Deleted event!"
-  });
+	res.send({"message": "Deleted event!"});
 })
 
 eventRouter.post('/insert', function(req, res) {
-  var event = new Event();
-  event.title = req.body.title;
+ 	var event = new Event();
+ 	event.title = req.body.title;
   event.description = req.body.description;
-  event.startTime = req.body.startTime;
-  event.endTime = req.body.endTime;
-  event.location = req.body.location;
-  event.grade = req.body.grade;
+ 	event.startTime = req.body.startTime;
+ 	event.endTime = req.body.endTime;
+ 	event.location = req.body.location;
+ 	event.grade = req.body.grade;
   event.imageURI = req.body.imageURI;
 
-  event.save(function(err) {
-    res.json({
-      message: 'Event created!',
-      event: event
-    });
-  });
-});
+ 	event.save(function(err) {
+ 		res.json({
+ 		  message: 'Event created!',
+ 	 	  event: event
+   });
+ 	});
+ });
 
-eventRouter.post('/edit', function(req, res) {
-  var eventToEdit = req.body;
+ eventRouter.post('/edit', function(req, res) {
+	var eventToEdit = req.body;
 
-  Event.findById(eventToEdit.id, function(err, event) {
-    event.set(eventToEdit);
-    event.save(function(err, updatedEvent) {
-      if (err) {
+	Event.findById(eventToEdit.id, function(err, event) {
+		event.set(eventToEdit);
+		event.save(function(err, updatedEvent) {
+			if (err) {
         return err.message;
       }
 
-      res.json({
-        message: 'Event updated!',
-        event: updatedEvent
-      });
-    });
-  });
+			res.json({
+				message: 'Event updated!',
+				event: updatedEvent
+			});
+		});
+	});
 });
 
-module.exports = eventRouter;
+ module.exports = eventRouter;
