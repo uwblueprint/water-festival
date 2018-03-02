776d32a10aa2681aa2b3d076455a586f52af9b5f
Amy Qiu <amy-qiu@outlook.com>
2018-03-02 10:07:36 -0500

    Fixed indenting in stylesheet

--- app/styles/HomeStyles.js
+++ app/styles/HomeStyles.js
@@ -3,10 +3,10 @@ import { StyleSheet } from 'react-native'
 const HomeStyles = StyleSheet.create({
 	faqLink: {
 		paddingTop: 20,
-    paddingLeft: 20,
-    fontSize: 20,
-    color: '#0000EE',
-    textDecorationLine: "underline"
+		paddingLeft: 20,
+		fontSize: 20,
+		color: '#0000EE',
+		textDecorationLine: "underline"
 	},
 });
 
