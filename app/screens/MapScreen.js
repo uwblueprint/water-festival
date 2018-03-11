7a7fd3be0c7ace86ab6594ea74386dc28af08f30
Varnit Grewal <varnitgrewal@v1020-wn-63-65.campus-dynamic.uwaterloo.ca>
2018-03-11 17:04:11 -0400

    Create get station coordinate function

--- app/screens/MapScreen.js
+++ app/screens/MapScreen.js
@@ -14,13 +14,164 @@ class MapScreen extends React.Component {
 	constructor(props) {
 		super(props);
 
+		const {x,y} = this.getStationCoord(1);
 		this.state = {
-			label: '',
-			x: 0,
-			y: 0
+			zoomFactor: 1,
+			x: 1000,
+			y: 1000
 		};
+		
+
+		console.log(x,y);
 
 		this.onClick = this.onClick.bind(this);
+	}
+
+	getStationCoord(stationNumber) {
+		let x = 0;
+		let y = 0;
+
+		if (stationNumber === 1) {
+			x = 388;
+			y = 480;
+		}
+
+		else if (stationNumber === 2) {
+			x = 310;
+			y = 450;
+		}
+
+		else if (stationNumber === 2.1) {
+			x = 530;
+			y = 265;
+		}
+
+		else if (stationNumber === 2.2) {
+			x = 685;
+			y = 285;
+		}
+
+		else if (stationNumber === 3) {
+			x = 200;
+			y = 370;
+		}
+
+		else if (stationNumber === 3.1) {
+			x = 555;
+			y = 210;
+		}
+
+		else if (stationNumber === 4) {
+			x = 615;
+			y = 245;
+		}
+
+		else if (stationNumber === 5) {
+			x = 675;
+			y = 170;
+		}
+
+		else if (stationNumber === 6) {
+			x = 600;
+			y = 100;
+		}
+
+		else if (stationNumber === 7) {
+			x = 520;
+			y = 100;
+		}
+
+		else if (stationNumber === 8) {
+			x = 440;
+			y = 170;
+		}
+
+		else if (stationNumber === 9) {
+			x = 515;
+			y = 215;
+		}
+
+		else if (stationNumber === 10) {
+			x = 380;
+			y = 215;
+		}
+
+		else if (stationNumber === 11) {
+			x = 268;
+			y = 230;
+		}
+
+		else if (stationNumber === 12) {
+			x = 275;
+			y = 215;
+		}
+
+		else if (stationNumber === 13) {
+			x = 280;
+			y = 215;
+		}
+
+		else if (stationNumber === 14) {
+			x = 200;
+			y = 280;
+		}
+
+		else if (stationNumber === 14.1) {
+			x = 575;
+			y = 160;
+		}
+
+		else if (stationNumber === 15) {
+			x = 180;
+			y = 230;
+		}
+
+		else if (stationNumber === 16) {
+			x = 190;
+			y = 195;
+		}
+
+		else if (stationNumber === 17) {
+			x = 135;
+			y = 308;
+		}
+
+		else if (stationNumber === 18) {
+			x = 165;
+			y = 315;
+		}
+
+		else if (stationNumber === 19) {
+			x = 140;
+			y = 390;
+		}
+
+		else if (stationNumber === 20) {
+			x = 300;
+			y = 385;
+		}
+
+		else if (stationNumber === 21) {
+			x = 350;
+			y = 365;
+		}
+
+		else if (stationNumber === 22) {
+			x = 278;
+			y = 445;
+		}
+
+		else if (stationNumber === 23) {
+			x = 660;
+			y = 390;
+		}
+
+		else if (stationNumber === 24) {
+			x = 605;
+			y = 350;
+		}
+
+		return {x, y};
 	}
 
 	onClick(evt) {
@@ -28,6 +179,8 @@ class MapScreen extends React.Component {
 		const x = evt.nativeEvent.locationX;
 		const y = evt.nativeEvent.locationY;
 		this.setState({ label: '' });
+
+		//return { x: 120, y: 50 };
 
 		// LANDMARK #1
 		if (x > 375 && x < 390 && y > 470 && y < 490) {
@@ -37,11 +190,11 @@ class MapScreen extends React.Component {
 		else if (x > 305 && x < 315 && y > 445 && y < 455) {
 			this.setState({ label: 'LANDMARK #2 >' });
 		}
-		// LANDMARK #2
+		// LANDMARK #2.1
 		else if (x > 535 && x < 545 && y > 260 && y < 270) {
 			this.setState({ label: 'LANDMARK #2 >' });
 		}
-		// LANDMARK #2
+		// LANDMARK #2.2
 		else if (x > 680 && x < 690 && y > 280 && y < 290) {
 			this.setState({ label: 'LANDMARK #2 >' });
 		}
@@ -49,7 +202,7 @@ class MapScreen extends React.Component {
 		else if (x > 190 && x < 210 && y > 365 && y < 375) {
 			this.setState({ label: 'LANDMARK #3 >' });
 		}
-		// LANDMARK #3
+		// LANDMARK #3.1
 		else if (x > 550 && x < 560 && y > 205 && y < 215) {
 			this.setState({ label: 'LANDMARK #3 >' });
 		}
@@ -97,7 +250,7 @@ class MapScreen extends React.Component {
 		else if (x > 190 && x < 210 && y > 275 && y < 285) {
 			this.setState({ label: 'LANDMARK #14 >' });
 		}
-		// LANDMARK #14
+		// LANDMARK #14.1
 		else if (x > 570 && x < 580 && y > 155 && y < 165) {
 			this.setState({ label: 'LANDMARK #14 >' });
 		}
@@ -151,7 +304,7 @@ class MapScreen extends React.Component {
 			type,
 			zoomCurrentDistance
 		} = data;
-
+		console.log(positionX, positionY)
 		// Only update when map stops moving
 		if (type !== "onPanResponderRelease") return;
 
@@ -167,6 +320,7 @@ class MapScreen extends React.Component {
 				imageWidth={ Dimensions.get('window').width }
 				imageHeight={ Dimensions.get('window').height }
 				onMove={ this.onMapPress }
+				centerOn={{ x: this.state.x, y: this.state.y, scale: this.state.zoomFactor }}
 			>
 				<Image
 					source={{ uri: 'https://water-fest.herokuapp.com/images/map.png' }}
